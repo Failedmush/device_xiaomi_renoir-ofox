beef227070baf0d1718665aaed7110d827546cf4
Failedmush <163705497+Failedmush@users.noreply.github.com>
2024-10-26 00:17:00 +0900

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -35,37 +35,45 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
- 	export ALLOW_MISSING_DEPENDENCIES=true
-	export TARGET_DEVICE_ALT="renoir,renoir_jp,renoir_cn,renoir_global"
-	export OF_TARGET_DEVICES="renoir_jp,renoir_cn,renoir_global,renoir"
-	export OF_VIRTUAL_AB_DEVICE=1
-	export OF_AB_DEVICE=1
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_USE_GREEN_LED=0
-	export OF_FBE_METADATA_MOUNT_IGNORE=1
+   	
+    # Version & Variant
+export FOX_VERSION="R12.1"
+export FOX_VARIANT="A14"
+export FOX_BUILD_TYPE="Stable"
+ # OrangeFox Addons
+export FOX_ENABLE_APP_MANAGER=1     
+    
+export TW_DEFAULT_LANGUAGE="en"
+export LC_ALL="C"
+ export ALLOW_MISSING_DEPENDENCIES=true
+export TARGET_DEVICE_ALT="renoir,renoir_jp,renoir_cn,renoir_global"
+export OF_TARGET_DEVICES="renoir_jp,renoir_cn,renoir_global,renoir"
+export OF_VIRTUAL_AB_DEVICE=1
+export OF_AB_DEVICE=1
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+export OF_USE_GREEN_LED=0
+export OF_FBE_METADATA_MOUNT_IGNORE=1
 
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_NEW_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-    export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_PATCH_AVB20=1
-    export FOX_DELETE_AROMAFM=1
-    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
-    export FOX_ENABLE_APP_MANAGER=0
-    export FOX_DISABLE_APP_MANAGER=1
+export OF_USE_MAGISKBOOT=1
+export OF_USE_NEW_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_NO_MIUI_PATCH_WARNING=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+export OF_QUICK_BACKUP_LIST="/boot;/data;"
+export OF_PATCH_AVB20=1
+export FOX_DELETE_AROMAFM=1
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+export FOX_ENABLE_APP_MANAGER=0
+export FOX_DISABLE_APP_MANAGER=1
 
     # OTA
     export OF_KEEP_DM_VERITY=1
